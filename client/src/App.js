58b268015d556060807261d5242862325423bedf
JRS296 <jonathansamuel296@gmail.com>
2022-07-14 20:26:14 +0530

    Commit #1 - POST request example - 14.07.2022 @20:26

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState} from "react";
+import Axios from "axios";
 
 function App() {
+  const [foodName, setFoodName] = useState("")
+  const [days, setDays] = useState(0)
+
+  const addToList = () => {
+    console.log(foodName+days)
+    Axios.post("http://localhost:3001/insert", {
+      foodName: foodName,
+      days: days,
+    });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>CRUD App using MERN</h1>
+
+      <label>Food Name:</label>
+      <input type="text" onChange={(e) => {setFoodName(e.target.value)}}></input>
+      <label>Days Elapsed:</label>
+      <input type="number" onChange={(e) => {setDays(e.target.value)}}></input>
+      <button onClick={addToList}>Add to List</button>
     </div>
   );
 }
--- server/index.js
+++ server/index.js
@@ -2,6 +2,14 @@ const express = require('express')
 const mongoose = require('mongoose')
 const app = express()
 
+const cors = require('cors');
+const corsOptions ={
+    origin:'http://localhost:3000', 
+    credentials:true,            //access-control-allow-credentials:true
+    optionSuccessStatus:200
+}
+app.use(cors(corsOptions));
+
 const FoodModel = require("./models/Food");
 
 app.use(express.json())
@@ -10,14 +18,18 @@ mongoose.connect("mongodb+srv://JRS296:pappa123@cluster0.gc5ji3s.mongodb.net/foo
     useNewUrlParser: true, 
 })
 
-app.get('/', async (req,res) => {
-    const food = new FoodModel({foodName: "Apple", daysSinceIAte: 3 });
+//POST Example
+app.post('/insert', async (req,res) => {
+    const fN = req.body.foodName
+    const ds = req.body.days
+    const food = new FoodModel({foodName: fN, daysSinceIAte: ds });
 
     try {
         console.log('Connected to MongoDB!!!')
         await food.save();
     } catch (error) {
-        console.log(error)
+        console.log(error);
+        console.log()
     }
 })
 
